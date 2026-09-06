b5fc5826ea3c70eac6a5a72b10a5e82467478806
David Cramer <dcramer@gmail.com>
2026-09-06 09:45:42 -0700

    Adopt Dynamic Type typography styles

--- Peated/Peated/Common/Extensions/Font+Peated.swift
+++ Peated/Peated/Common/Extensions/Font+Peated.swift
@@ -2,26 +2,26 @@ import SwiftUI
 
 extension Font {
     // Hanken Grotesk/Karla equivalents until the web font files are shipped in the app bundle.
-    static let peatedLargeTitle = Font.system(size: 40, weight: .bold, design: .rounded)
-    static let peatedTitle = Font.system(size: 32, weight: .bold, design: .rounded)
-    static let peatedTitle2 = Font.system(size: 22, weight: .bold, design: .rounded)
-    static let peatedTitle3 = Font.system(size: 20, weight: .bold, design: .rounded)
-    static let peatedHeadline = Font.system(size: 17, weight: .semibold)
-    static let peatedBody = Font.system(size: 17, weight: .regular)
-    static let peatedCallout = Font.system(size: 16, weight: .regular)
-    static let peatedSubheadline = Font.system(size: 15, weight: .regular)
-    static let peatedFootnote = Font.system(size: 13, weight: .regular)
-    static let peatedCaption = Font.system(size: 12, weight: .regular)
-    static let peatedCaption2 = Font.system(size: 11, weight: .regular)
+    static let peatedLargeTitle = Font.system(.largeTitle, design: .rounded, weight: .bold)
+    static let peatedTitle = Font.system(.title, design: .rounded, weight: .bold)
+    static let peatedTitle2 = Font.system(.title2, design: .rounded, weight: .bold)
+    static let peatedTitle3 = Font.system(.title3, design: .rounded, weight: .bold)
+    static let peatedHeadline = Font.system(.headline, weight: .semibold)
+    static let peatedBody = Font.system(.body)
+    static let peatedCallout = Font.system(.callout)
+    static let peatedSubheadline = Font.system(.subheadline)
+    static let peatedFootnote = Font.system(.footnote)
+    static let peatedCaption = Font.system(.caption)
+    static let peatedCaption2 = Font.system(.caption2)
 
     // Kept as source-compatible names while adopting the new sans-serif display role.
-    static let peatedDisplaySerif = Font.system(size: 24, weight: .bold, design: .rounded)
-    static let peatedDisplaySerifLarge = Font.system(size: 32, weight: .bold, design: .rounded)
-    static let peatedHeadlineSerif = Font.system(size: 18, weight: .bold, design: .rounded)
+    static let peatedDisplaySerif = Font.system(.title2, design: .rounded, weight: .bold)
+    static let peatedDisplaySerifLarge = Font.system(.title, design: .rounded, weight: .bold)
+    static let peatedHeadlineSerif = Font.system(.headline, design: .rounded, weight: .bold)
 
     /// Single semantic title for screen headers (Profile/Bottle/Entity)
-    static let titlePrimary = Font.system(size: 22, weight: .bold, design: .rounded)
+    static let titlePrimary = Font.system(.title2, design: .rounded, weight: .bold)
 
     /// Semantic alias for prominent names (bottles, entities, usernames)
-    static let nameTitle = Font.system(size: 18, weight: .bold, design: .rounded)
+    static let nameTitle = Font.system(.headline, design: .rounded, weight: .bold)
 }
